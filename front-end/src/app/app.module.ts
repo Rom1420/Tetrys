e1868a5d5ebc5fad28df64acfb9d5063ddaf34e2
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-03-19 06:50:02 +0100

    Feat : start adding the difficulty buttons

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './nav-bar/nav-bar.component';
+import { DifficultyButtonComponent } from './difficulty-button/difficulty-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DifficultyButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/src/app/difficulty-button/difficulty-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'difficulty-button',
+    templateUrl: './difficulty-button.component.html',
+    styleUrls: ['./difficulty-button.component.scss']
+})
+
+export class DifficultyButtonComponent implements OnInit {
+    constructor() {}
+    ngOnInit(): void {}
+} 
