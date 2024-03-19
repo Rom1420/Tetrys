ec55efb0a386b6d6af0cacd9c1ba4cb42ba4a53c
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-03-19 06:27:40 +0100

    Feat added the NavBar

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { Component } from './name/name.component'; to import component
 
-const routes: Routes = [];
+const routes: Routes = [
+  /**{path: 'quiz', component: QuizComponent} To create route to this component*/ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
