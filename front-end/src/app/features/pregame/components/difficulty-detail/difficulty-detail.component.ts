3fddc67f5efab1218c37e488f049b3fb39232945
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-03-30 23:07:58 +0100

    Feat : start the difficulty detail component

---
+++ front-end/src/app/features/pregame/components/difficulty-detail/difficulty-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'difficulty-detail',
+  templateUrl: './difficulty-detail.component.html',
+  styleUrl: './difficulty-detail.component.scss'
+})
+export class DifficultyDetailComponent {
+
+}
