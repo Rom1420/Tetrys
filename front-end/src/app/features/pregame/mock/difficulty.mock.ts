a72c450d65ff34e23b22263ba7ec3ef694043371
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-03-30 23:09:47 +0100

    Feat : added on the pregame module the difficulty detail component without getting the detail from the mock

--- front-end/src/app/features/pregame/mock/difficulty.mock.ts
+++ front-end/src/app/features/pregame/mock/difficulty.mock.ts
@@ -4,20 +4,40 @@ export const DIFFICULTY_LIST: Difficulty[] = [
         id: 1,
         name: 'Débutant',
         description: 'Le mode pas trop dur',
+        details: {
+            timePerCharacter: 10,
+            maxWordLength: 8,
+            allowErrors: true
+        }
     },
     {
         id: 2,
         name: 'Intermédiaire',
         description: 'Le mode un peu dur',
+        details: {
+            timePerCharacter: 10,
+            maxWordLength: 8,
+            allowErrors: true
+        }
     },
     {
         id: 3,
         name: 'Avancé',
         description: 'Le mode dur',
+        details: {
+            timePerCharacter: 10,
+            maxWordLength: 8,
+            allowErrors: true
+        }
     },
     {
         id: 4,
         name: 'Personnalisé',
         description: 'Le mode Perso',
+        details: {
+            timePerCharacter: 10,
+            maxWordLength: 8,
+            allowErrors: true
+        }
     }
 ]
