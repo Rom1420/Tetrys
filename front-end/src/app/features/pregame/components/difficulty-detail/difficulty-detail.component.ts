619c839c3cf870f8405af9fc4feffa75f1e8324e
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-03-31 01:13:11 +0100

    Feat : add some styles

--- front-end/src/app/features/pregame/components/difficulty-detail/difficulty-detail.component.ts
+++ front-end/src/app/features/pregame/components/difficulty-detail/difficulty-detail.component.ts
@@ -8,4 +8,19 @@ import { Difficulty } from '../../models/difficulty.model';
 })
 export class DifficultyDetailComponent {
   @Input() currentDifficulty: Difficulty | null = null;
+
+  getDifficultyClass(): string {
+    switch (this.currentDifficulty?.id) {
+        case 1:
+            return 'debutant';
+        case 2:
+            return 'intermediaire';
+        case 3:
+            return 'avance';
+        case 4:
+            return 'perso';
+        default:
+            return '';
+    }
+}
 }
