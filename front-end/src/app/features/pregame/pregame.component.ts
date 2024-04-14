25f390ed0909f95d904a8abec7a09832ba4b28ee
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-04-14 21:33:19 +0200

    Feat : added some features in the pregame module

--- front-end/src/app/features/pregame/pregame.component.ts
+++ front-end/src/app/features/pregame/pregame.component.ts
@@ -1,6 +1,7 @@
-import { Component, Output } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { PopupService } from './services/popup.service';
 import { Difficulty } from './models/difficulty.model';
+import { StudentService } from 'src/app/core/components/services/student.service';
 
 @Component({
   selector: 'app-pregame',
@@ -8,17 +9,46 @@ import { Difficulty } from './models/difficulty.model';
   styleUrl: './pregame.component.scss',
 
 })
-export class PregameComponent {
-  constructor(public popupService: PopupService){}
+export class PregameComponent implements OnInit{
 
+  isHovering: boolean = false;
+
+  selectedPlayerId: number | null = null;
   selectedDifficulty: Difficulty | null = null;
+  isMidContainerHovered: boolean = false;
 
+  constructor(public popupService: PopupService,
+    private studentService: StudentService
+  ){}
+
+
+  ngOnInit(): void {
+    this.studentService.selectedStudentId$.subscribe((studentId: number) => {
+      if (studentId) {
+        this.selectedPlayerId = studentId; 
+      } else {
+        this.selectedPlayerId = null;
+      }
+    }); 
+  }
+
+  onMidContainerHover(): void {
+    this.isMidContainerHovered = true;
+  }
+
+  onMidContainerHoverEnd(): void {
+    this.isMidContainerHovered = false;
+  }
 
   onDifficultyHover(difficulty: Difficulty): void {
-    this.selectedDifficulty = difficulty;
+      this.isHovering = true;
+    if(this.selectedPlayerId != null){
+      this.selectedDifficulty = difficulty;
+    } 
   }
 
   onDifficultyHoverEnd(): void {
+    this.isHovering = false;
     this.selectedDifficulty = null;
   }
 }
