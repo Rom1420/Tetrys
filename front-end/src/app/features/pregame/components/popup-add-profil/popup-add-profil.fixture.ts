6ee1d0e8bd2d61be0c431d3b3ff3282478dcb396
Mathias Santos Reis <mathias.santos-reis@etu.univ-cotedazur.fr>
2024-06-17 14:45:28 +0200

    fixture of addprofil popupadd and student

---
+++ front-end/src/app/features/pregame/components/popup-add-profil/popup-add-profil.fixture.ts
@@ -0,0 +1,17 @@
+import { E2EComponentFixture } from "e2e/e2e-component.fixture";
+export class ProfilFormFixture extends E2EComponentFixture {
+
+    getProfilForm() {
+        return this.page.waitForSelector('popup-add-profil');
+    }
+    getInput(id: string){
+        const selector = `popup-add-profil input[id="${id}"]`;
+        return this.page.waitForSelector(selector);
+    }
+    getCreateButton(){
+        return this.page.getByRole('button', {name: "&#10003;"});
+    }
+    clickCreateButton() {
+        return this.getCreateButton().click();
+    }
+}
---
+++ front-end/src/app/features/pregame/components/student/student.fixture.ts
@@ -0,0 +1,46 @@
+import { E2EComponentFixture } from "e2e/e2e-component.fixture";
+
+export class StudentFixture extends E2EComponentFixture {
+    async getNameStudent(index: number) {
+        const allTitles = await this.getAllNames();
+        if (index >= allTitles.length) {
+          throw new Error("Wrong Title Quiz Index");
+        }
+        return allTitles[index];
+    }
+    
+    async getContentNameStudent(index: number) {
+        const name = await this.getNameStudent(index);
+        return name.textContent();
+    }
+    getAllNames(){
+        return this.page.$$('student p');
+    }
+    getDelButton(){
+        return this.page.getByTestId('closeButton');
+    }
+    clickDelButton(){
+        return this.getDelButton().click();
+    }
+    async getIndexOfName(name: string) {
+        const names = await this.getAllNames();
+        let indexOfCard = -1;
+        for (let index = 0; index < names.length; index++) {
+          if ((await names[index].textContent()) == name) {
+            indexOfCard = index;
+          }
+        }
+        return indexOfCard;
+    }
+    async selectStudent(name: string){
+        const selector = `div:has-text("${name}")`;
+        const allSelectors = await this.page.$$(selector);
+        const indexOfStudent = await this.getIndexOfName(name);
+
+        if(indexOfStudent>=allSelectors.length){
+            throw new Error(`Wrong Student Name`);
+        }
+        await allSelectors[indexOfStudent].click()
+
+    }
+}
