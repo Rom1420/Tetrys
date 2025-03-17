68c36d4c24946db4cb8b9bac1234563d9173522f
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2025-03-17 13:54:45 +0100

    Modifications pour deployer

--- front-end/src/environnement/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  backUrl: 'http://ton-backend.aws-region.compute.amazonaws.com:9428/api'
-};
--- front-end/src/environnement/environnement-prod.ts
+++ front-end/src/environnement/environnement-prod.ts
@@ -1,1 +1,1 @@
-export const backUrl = 'http://localhost:9428/api';
+export const backUrl = 'http://13.39.86.218:9428/api';
